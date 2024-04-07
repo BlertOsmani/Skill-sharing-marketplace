2190dbc73304a2b1b49e4110bfef8f9c8fb91be0
Blert Osmani <blertosmani15@gmail.com>
2024-04-07 19:53:48 +0200

    envsetup/SSM-47-Install-PrimeVue-as-a-module-for-the-UI-of-the-application: Installed PrimeVue modules for building the UI of the application. Made some changes in the App.vue file to see if the primevue modules are working (Just added an inputtext). Everything worked great

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
+import PrimeVue from 'primevue/config'
 import App from './App.vue'
+import InputText from 'primevue/inputtext';
+//in main.js
+import 'primevue/resources/themes/aura-dark-green/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css'
 
-createApp(App).mount('#app')
+
+const app = createApp(App);
+app.use(PrimeVue);
+app.component('InputText', InputText);
+app.mount('#app');
