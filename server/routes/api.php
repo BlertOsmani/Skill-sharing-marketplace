e11c814ca31e885cb6d45ad12a4835baa64046d8
Blert Osmani <blertosmani15@gmail.com>
2024-04-11 00:00:22 +0200

    eature/SSM-32-Develop-backend-API-endpoint-for-user-registration: Changed the name of the function that will be executed in the backend in the api.php file when a user wants to register

--- server/routes/api.php
+++ server/routes/api.php
@@ -8,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/user/create', [UserController::class, 'store']);
+Route::post('/user/create', [UserController::class, 'register']);
