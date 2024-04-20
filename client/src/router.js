5326084b3722e3ea4a1bf25a55936ba0498f62cb
diartmaraj <118026687+diartmaraj@users.noreply.github.com>
2024-04-20 18:33:44 +0200

    feature/SSM-36-Create-login-form:Created LoginForm component and Created Login view.

--- client/src/router.js
+++ client/src/router.js
@@ -1,11 +1,13 @@
 // src/router.js
-import { createRouter, createWebHistory } from 'vue-router';
-import Register from './views/Register.vue';
-import Discover from './views/Discover.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Register from "./views/Register.vue";
+import Discover from "./views/Discover.vue";
+import Login from "./views/Login.vue";
 
 const routes = [
-  { path: '/register', component: Register },
-  {path : '/', component: Discover}
+  { path: "/register", component: Register },
+  { path: "/", component: Discover },
+  { path: "/login", component: Login },
   // Add other routes here
 ];
 
