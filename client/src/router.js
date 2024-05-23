e560dc310d7a37ff86ac581f9a3948769f25e685
Astrit Krasniqi <121978549+astritkrasniqi1@users.noreply.github.com>
2024-05-23 14:38:19 +0200

    feature/SSM-20-Course-discovery: Added a new route in the client side.

--- client/src/router.js
+++ client/src/router.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Register from './views/Register.vue';
 import Discover from './views/Discover.vue';
 import Search from './views/Search.vue';
+import CourseDetails from './views/CourseDetails.vue';
 
 const routes = [
   { path: '/register', component: Register },
   {path : '/', component: Discover},
-  {path: '/search', component: Search}
+  {path: '/search', component: Search},
+  {path: '/coursedetails', component: CourseDetails}
 ];
 
 const router = createRouter({
